3a6c0d3e2813f47cbeae37b6e2228d8975f64141
Sven Van Caekenberghe <sven@beta9.be>
2023-12-16 21:23:02 +0100

    Add constraint drop statements

--- f1db_constraints.sql
+++ f1db_constraints.sql
@@ -7,9 +7,15 @@
 --
 
 ALTER TABLE constructorresults
+DROP CONSTRAINT IF EXISTS constructorresults_races_fkey;
+
+ALTER TABLE constructorresults
 ADD CONSTRAINT constructorresults_races_fkey
 FOREIGN KEY (raceid)
 REFERENCES races(raceid);
+
+ALTER TABLE constructorresults
+DROP CONSTRAINT IF EXISTS constructorresults_constructors_fkey;
 
 ALTER TABLE constructorresults
 ADD CONSTRAINT constructorresults_constructors_fkey
@@ -21,9 +27,15 @@ REFERENCES constructors(constructorid);
 --
 
 ALTER TABLE constructorstandings
+DROP CONSTRAINT IF EXISTS constructorstandings_races_fkey;
+
+ALTER TABLE constructorstandings
 ADD CONSTRAINT constructorstandings_races_fkey
 FOREIGN KEY (raceid)
 REFERENCES races(raceid);
+
+ALTER TABLE constructorstandings
+DROP CONSTRAINT IF EXISTS constructorstandings_constructors_fkey;
 
 ALTER TABLE constructorstandings
 ADD CONSTRAINT constructorstandings_constructors_fkey
@@ -55,9 +67,15 @@ REFERENCES drivers(driverid);
 --
 
 ALTER TABLE results
+DROP CONSTRAINT IF EXISTS results_races_fkey;
+
+ALTER TABLE results
 ADD CONSTRAINT results_races_fkey
 FOREIGN KEY (raceid)
 REFERENCES races(raceid);
+
+ALTER TABLE results
+DROP CONSTRAINT IF EXISTS results_drivers_fkey;
 
 ALTER TABLE results
 ADD CONSTRAINT results_drivers_fkey
@@ -65,9 +83,15 @@ FOREIGN KEY (driverid)
 REFERENCES drivers(driverid);
 
 ALTER TABLE results
+DROP CONSTRAINT IF EXISTS results_constructors_fkey;
+
+ALTER TABLE results
 ADD CONSTRAINT results_constructors_fkey
 FOREIGN KEY (constructorid)
 REFERENCES constructors(constructorid);
+
+ALTER TABLE results
+DROP CONSTRAINT IF EXISTS results_status_fkey;
 
 ALTER TABLE results
 ADD CONSTRAINT results_status_fkey
@@ -79,9 +103,15 @@ REFERENCES status(statusid);
 --
 
 ALTER TABLE sprintresults
+DROP CONSTRAINT IF EXISTS sprintresults_races_fkey;
+
+ALTER TABLE sprintresults
 ADD CONSTRAINT sprintresults_races_fkey
 FOREIGN KEY (raceid)
 REFERENCES races(raceid);
+
+ALTER TABLE sprintresults
+DROP CONSTRAINT IF EXISTS sprintresults_drivers_fkey;
 
 ALTER TABLE sprintresults
 ADD CONSTRAINT sprintresults_drivers_fkey
@@ -89,9 +119,15 @@ FOREIGN KEY (driverid)
 REFERENCES drivers(driverid);
 
 ALTER TABLE sprintresults
+DROP CONSTRAINT IF EXISTS sprintresults_constructors_fkey;
+
+ALTER TABLE sprintresults
 ADD CONSTRAINT sprintresults_constructors_fkey
 FOREIGN KEY (constructorid)
 REFERENCES constructors(constructorid);
+
+ALTER TABLE sprintresults
+DROP CONSTRAINT IF EXISTS sprintresults_status_fkey;
 
 ALTER TABLE sprintresults
 ADD CONSTRAINT sprintresults_status_fkey
@@ -103,14 +139,23 @@ REFERENCES status(statusid);
 --
 
 ALTER TABLE qualifying
+DROP CONSTRAINT IF EXISTS qualifying_races_fkey;
+
+ALTER TABLE qualifying
 ADD CONSTRAINT qualifying_races_fkey
 FOREIGN KEY (raceid)
 REFERENCES races(raceid);
 
 ALTER TABLE qualifying
+DROP CONSTRAINT IF EXISTS qualifying_drivers_fkey;
+
+ALTER TABLE qualifying
 ADD CONSTRAINT qualifying_drivers_fkey
 FOREIGN KEY (driverid)
 REFERENCES drivers(driverid);
+
+ALTER TABLE qualifying
+ADD CONSTRAINT qualifying_constructors_fkey;
 
 ALTER TABLE qualifying
 ADD CONSTRAINT qualifying_constructors_fkey
@@ -122,9 +167,15 @@ REFERENCES constructors(constructorid);
 --
 
 ALTER TABLE pitstops
+DROP CONSTRAINT IF EXISTS pitstops_races_fkey;
+
+ALTER TABLE pitstops
 ADD CONSTRAINT pitstops_races_fkey
 FOREIGN KEY (raceid)
 REFERENCES races(raceid);
+
+ALTER TABLE pitstops
+DROP CONSTRAINT IF EXISTS pitstops_drivers_fkey;
 
 ALTER TABLE pitstops
 ADD CONSTRAINT pitstops_drivers_fkey
@@ -136,9 +187,15 @@ REFERENCES drivers(driverid);
 --
 
 ALTER TABLE laptimes
+DROP CONSTRAINT IF EXISTS laptimes_races_fkey;
+
+ALTER TABLE laptimes
 ADD CONSTRAINT laptimes_races_fkey
 FOREIGN KEY (raceid)
 REFERENCES races(raceid);
+
+ALTER TABLE laptimes
+DROP CONSTRAINT IF EXISTS laptimes_drivers_fkey;
 
 ALTER TABLE laptimes
 ADD CONSTRAINT laptimes_drivers_fkey
@@ -150,9 +207,15 @@ REFERENCES drivers(driverid);
 --
 
 ALTER TABLE races
+DROP CONSTRAINT IF EXISTS races_circuits_fkey;
+
+ALTER TABLE races
 ADD CONSTRAINT races_circuits_fkey
 FOREIGN KEY (circuitid)
 REFERENCES circuits(cirtcuitid);
+
+ALTER TABLE races
+DROP CONSTRAINT IF EXISTS races_seasons_fkey;
 
 ALTER TABLE races
 ADD CONSTRAINT races_seasons_fkey
