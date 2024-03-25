55b3bc5c07672fa9b12f1b88a024e12d10b5ff40
svenvc <sven@stfx.eu>
2024-03-25 11:44:48 +0100

    add dbname to script

--- 01-setup.sh
+++ 01-setup.sh
@@ -3,7 +3,7 @@ set -e
 
 export PGPASSWORD=tiger
 
-curl -s -L https://github.com/svenvc/F1-MRD-Database-PSQL/raw/main/f1db_dump.sql.bz2 | bunzip2 | psql -U scott -f -
+curl -s -L https://github.com/svenvc/F1-MRD-Database-PSQL/raw/main/f1db_dump.sql.bz2 | bunzip2 | psql -U scott -f - f1db
 
-curl -s -L https://github.com/svenvc/F1-MRD-Database-PSQL/raw/main/f1db_constraints.sql | psql -U scott -f -
+curl -s -L https://github.com/svenvc/F1-MRD-Database-PSQL/raw/main/f1db_constraints.sql | psql -U scott -f - f1db
  
